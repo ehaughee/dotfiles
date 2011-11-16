c517ca6d8498fa65e917609c03f6e125dda32cfd
Eric Haughee <ehaughee@gmail.com>
2011-11-16 15:02:09 -0500

    Commenting and preamble cleanup

--- tmuxgo.sh
+++ tmuxgo.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 ################################################################################
-# tmuxgo - Dale Bewley <dale @ guifreelife org> - Sat Feb 19 08:53:30 PST 2011
+# Genearl structure and foundation from:
+# tmuxgo - Dale Bewley <dale @ guifreelife org>
 #-------------------------------------------------------------------------------
-# Use me to get your tmux session restored after a reboot or reattach daily.
-# Just type tmuxgo every morning and hit ^bd at the end of the day. Login
-# remotely and do the same.
-#
-# Attaches to an existing session named $SESSION or will create one if missing.
-# The created session will be pre-populated with a number of windows. 
-#
-# For example, window 0 running IRC, window 1 running email, window 2 logged
-# into a router used daily.
-#
-#
 # Bugs & Todos:
 #   o If session already exists, instantiate any missing windows.
 #     This could be done by checking tmux list-windows, not sure needed.
@@ -24,11 +14,11 @@
 #     Note that name (#W) and title (#T) are not necessarily the same value.
 ################################################################################
  
-# the name of your primary tmux session
+# the name of your primary tmux session, defaults to current username
 SESSION=$USER
  
 # if the session is already running, just attach to it.
-
+# send server not found errors to /dev/null
 if tmux has-session -t "$SESSION" 2>/dev/null; then
     echo "Session $SESSION already exists. Attaching..."
     sleep 1
@@ -36,12 +26,13 @@ if tmux has-session -t "$SESSION" 2>/dev/null; then
     exit 0;
 fi
                  
-# create a new session, named $SESSION, and detach from it
+# find the terminal's width and height so that splitting panes are sized properly
 set -- $(stty size) # $1 = rows $2 = columns
+
+# create a new detached session, named $SESSION
 tmux -2 new-session -d -s "$SESSION" -x "$2" -y "$(($1-1))"
 
 # Now populate the session with the windows you use every day
-
 # 0 - VIM
 tmux new-window  -t "$SESSION":0 -k -n VIM
 tmux send-keys   -t "$SESSION":0 'cd /home/juice/juice38/' C-m
@@ -54,11 +45,9 @@ tmux select-pane -t 0
 # 1 - MySQL
 tmux new-window -t "$SESSION":1 -k -n MySQL 'mysql -u root -p'
 
-# 2 - Shell
+# 2 - Root Shell
 tmux new-window -t "$SESSION":2 -k -n Shell 'sudo -s'
 
-# all done. select starting window and get to work
-# you may need to cycle through windows and type in passwords
-# if you don't use ssh keys
+# Select the starting window, and attach to the session
 tmux select-window -t "$SESSION":0
 tmux -2 attach -t "$SESSION"
